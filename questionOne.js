24b0af3d12b11081e7f42b285a2733110eaa5c0e
Lindsay Maher <lindsaymah3r@gmail.com>
2018-06-11 23:42:45 -0400

    adds more to first problem

--- questionOne.js
+++ questionOne.js
@@ -1,11 +1,12 @@
 // Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
 
 const sortByStrings = (stringOne, stringTwo) => {
-  //for stringTwo, loop through and store each letter with an incrementing counter as the key and the letter as the value. 
+
   const dictionary = stringTwo.split('')
   let reOrderedWord = {}
 
   stringOne.split('').forEach((letter) => {
+
     if (stringTwo.indexOf(letter) > -1) {
       if (reOrderedWord[stringTwo.indexOf(letter)]) {
         reOrderedWord[stringTwo.indexOf(letter)] += letter
@@ -15,6 +16,12 @@ const sortByStrings = (stringOne, stringTwo) => {
 
     }
   })
+
+  let newString = ''
+  for (let i = 0; i < stringTwo.length; i++) {
+    if (reOrderedWord[i]) newString += reOrderedWord[i]
+  }
+  return newString
 
 }
 
