556ca748781bb6af8fdb78a3421144563b6f995e
Lindsay Maher <lindsaymah3r@gmail.com>
2018-06-12 21:19:53 -0400

    cleans up console.logs

--- questionTwo.js
+++ questionTwo.js
@@ -5,65 +5,43 @@
 // For s = "4[ab]", the output should be decodeString(s) = "abababab" 
 // For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
 
-
-const decodeString = (str) => {
-  let count = 1
-  let multiplier = str[0]
-  let newString = ''
-
-  let encodedStr = str.slice(str.indexOf('[') + 1, str.indexOf(']'))
-  console.log('this is the inner encoded string', encodedStr)
-
-  while (count <= multiplier) {
-    console.log(multiplier)
-    newString += encodedStr
-    console.log(encodedStr)
-    count++
-  }
-  return newString
-}
-
 const decodeString = (str) => {
   let multiplier = 0,
     newString = '',
     startString = 0,
     endString = 0
 
-  //  let encodedStr = str.slice(str.indexOf('[') + 1, str.indexOf(']'))
-  // console.log('this is the inner encoded string', encodedStr)
   if (str.indexOf('[') === -1) {
     newString = str.slice(0, str.length - 1)
-    // console.log(newString)
     return newString
   }
   else {
+
     for (let i = 0; i < str.length; i++) {
       if (Number(str[i])) {
         multiplier = +str[i]
-        console.log(multiplier)
-        console.log(typeof multiplier)
       }
 
       if (str[i] === '[') {
-        console.log(str[i])
         startString = i + 1
       }
       if (str[i] === ']') {
-        console.log(str[i])
         endString = i
         break;
       }
     }
-    console.log('this is the multiplier', multiplier)
+
     for (let j = 0; j < multiplier; j++) {
-      console.log('is this running')
       let strRepeat = str.slice(startString, endString)
       newString += strRepeat
     }
-    console.log(multiplier)
-    // console.log(str.lastIndexOf(multiplier))
-    console.log("this is a string", str.slice(0, str.lastIndexOf(multiplier)))
+
     return decodeString(str.slice(0, str.lastIndexOf(multiplier)) + newString + str.slice(str.length - 1))
   }
 
 }
+
+
+//test cases
+// console.log(decodeString("4[ab]")) //'abababab'
+// console.log(decodeString("2[b3[a]]")) // "baaabaaa"
