47f402e99014bd715b84fbed6fa76f9e87409e8b
Lindsay Maher <lindsaymah3r@gmail.com>
2018-06-12 21:08:39 -0400

    completes second problem, adds work from repl

--- questionTwo.js
+++ questionTwo.js
@@ -7,6 +7,63 @@
 
 
 const decodeString = (str) => {
-  //split the inputed string each time there are bracketts and add the inner value to a string according to the 
-  //number (k). if it's a nested encoder, recurse and add those values to the newStr. 
+  let count = 1
+  let multiplier = str[0]
+  let newString = ''
+
+  let encodedStr = str.slice(str.indexOf('[') + 1, str.indexOf(']'))
+  console.log('this is the inner encoded string', encodedStr)
+
+  while (count <= multiplier) {
+    console.log(multiplier)
+    newString += encodedStr
+    console.log(encodedStr)
+    count++
+  }
+  return newString
+}
+
+const decodeString = (str) => {
+  let multiplier = 0,
+    newString = '',
+    startString = 0,
+    endString = 0
+
+  //  let encodedStr = str.slice(str.indexOf('[') + 1, str.indexOf(']'))
+  // console.log('this is the inner encoded string', encodedStr)
+  if (str.indexOf('[') === -1) {
+    newString = str.slice(0, str.length - 1)
+    // console.log(newString)
+    return newString
+  }
+  else {
+    for (let i = 0; i < str.length; i++) {
+      if (Number(str[i])) {
+        multiplier = +str[i]
+        console.log(multiplier)
+        console.log(typeof multiplier)
+      }
+
+      if (str[i] === '[') {
+        console.log(str[i])
+        startString = i + 1
+      }
+      if (str[i] === ']') {
+        console.log(str[i])
+        endString = i
+        break;
+      }
+    }
+    console.log('this is the multiplier', multiplier)
+    for (let j = 0; j < multiplier; j++) {
+      console.log('is this running')
+      let strRepeat = str.slice(startString, endString)
+      newString += strRepeat
+    }
+    console.log(multiplier)
+    // console.log(str.lastIndexOf(multiplier))
+    console.log("this is a string", str.slice(0, str.lastIndexOf(multiplier)))
+    return decodeString(str.slice(0, str.lastIndexOf(multiplier)) + newString + str.slice(str.length - 1))
+  }
+
 }
